bf0e106c9b346f5ea094fbb3d43a1e4ab9386bd1
Alex4J <monkeycoin@protonmail.ch>
2016-01-19 10:36:10 +0100

    changing unused variable

--- src/wallet.cpp
+++ src/wallet.cpp
@@ -1627,7 +1627,7 @@ bool CWallet::CreateCoinStake(const CKeyStore& keystore, unsigned int nBits, int
     }
     // Calculate reward
     {
-        uint64 nReward = GetProofOfStakeReward(nCoinAge, nBits, txNew.nTime, pindexBest->nHeight, pindexBest->nMoneySupply);
+        uint64 nReward = GetProofOfStakeReward(0, nBits, txNew.nTime, pindexBest->nHeight, pindexBest->nMoneySupply);
         if (nReward <= 0)
             return false;
 
